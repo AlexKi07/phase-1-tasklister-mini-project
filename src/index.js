7d4d6021dfd329d3396ee3efb5d56290f084ecac
AlexKi07 <kimanialexk07@gmail.com>
2025-03-16 15:29:07 +0300

    Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const formInput = document.querySelector("#new-task-description");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+
+    const newTask = document.createElement("li");
+    newTask.textContent = formInput.value;
+
+    taskList.appendChild(newTask);
+
+
+    formInput.value = ""
+  })
 });
